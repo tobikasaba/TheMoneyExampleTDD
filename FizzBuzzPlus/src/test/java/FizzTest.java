c980266b4b494a169dacba259205862bc66c9ed2
Tobi Kasaba <tkasaba@deloitte.co.uk>
2023-03-13 14:17:33 +0000

    First test for FizzBuzzPlus exercise

--- FizzBuzzPlus/src/test/java/FizzTest.java
+++ FizzBuzzPlus/src/test/java/FizzTest.java
@@ -1,2 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzTest {
+//    List<Integer> oneToHundredTest = new ArrayList<>();
+
+    @Test
+    void testRangeOfNumbers() {
+        List<Integer> oneToHundredTest = new ArrayList<>();
+        FizzClass oneToHundred = new FizzClass();
+        int startingNumber = 1;
+        int endNumber = 100;
+        for (int i = startingNumber; i < endNumber; i++) {
+            oneToHundredTest.add(i);
+        }
+        Assertions.assertEquals(oneToHundredTest, oneToHundred.numberRange(startingNumber, endNumber));
+    }
 }
