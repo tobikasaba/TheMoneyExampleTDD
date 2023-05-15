0b393fe967c49f80086a5980d72e7710be74b400
Tobi Kasaba <tkasaba@deloitte.co.uk>
2023-05-15 15:51:26 +0100

    refactored names of methods in test to make them short and concise removed duplication in implementation by adding looping method

--- FileNameRange/src/main/java/FileName.java
+++ FileNameRange/src/main/java/FileName.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * FileName class checks the index of files and add the first and last index of the keywords before the file extension from
  * the files name to an array.
@@ -11,25 +14,30 @@ public class FileName {
 
     public static int keyWords(String filePath) {
         filePath = filePathLowerCase(filePath);
-        if (filePath.contains("spec")) {
-            return filePath.lastIndexOf("spec");
-        } else if (filePath.contains("test")) {
-            return filePath.lastIndexOf("test");
-        } else {
-            return filePath.lastIndexOf("step");
-        }
+        List<String> filePathString = Arrays.asList("spec", "test", "step");
+        return loopMethod(filePath, filePathString);
     }
 
     public static int separatorCharacters(String filePath) {
         filePath = filePathLowerCase(filePath);
         if (filePath.contains("-")) {
             return filePath.lastIndexOf("-");
-        } else if(filePath.contains("_")){
+        } else if (filePath.contains("_")) {
             return filePath.lastIndexOf("_");
-        }else{
+        } else {
             String[] subWords = filePath.split("\\.");
             return filePath.lastIndexOf("." + subWords[subWords.length - 2]);
         }
+
+    }
+
+    public static int loopMethod(String filePath, List<String> filePathString) {
+        for (String i : filePathString) {
+            if (filePath.contains(i)) {
+                return filePath.lastIndexOf(i);
+            }
+        }
+        return -1;
     }
 
     public Selection select(String filePath) {
--- FileNameRange/src/test/java/FileNameTest.java
+++ FileNameRange/src/test/java/FileNameTest.java
@@ -23,39 +23,52 @@ public class FileNameTest {
     }
 
     @Test
-    void selectFileNameWithNoExtension() {
+    void removesSpecAndExtension() {
         assertEquals(Selection.of(0,7),fileName.select("DiamondSpec.kt"));
         System.out.println(fileName.select("DiamondSpec.kt"));
     }
 
     @Test
-    void selectFileNameIfInADirectory(){
+    void removesTestAndExtension() {
+        assertEquals(Selection.of(0,7),fileName.select("DiamondTest.kt"));
+        System.out.println(fileName.select("DiamondTest.kt"));
+    }
+    @Test
+    void removesTestsAndExtension() {
+        assertEquals(Selection.of(0,7),fileName.select("DiamondTests.kt"));
+        System.out.println(fileName.select("DiamondTests.kt"));
+    }
+
+    @Test
+    void removesStepAndExtension() {
+        assertEquals(Selection.of(0,7),fileName.select("DiamondStep.kt"));
+        System.out.println(fileName.select("DiamondStep.kt"));
+    }
+
+    @Test
+    void removesDirectoryAndExtension(){
         assertEquals(Selection.of(12,17),fileName.select("wibble/test/hiker.rb"));
         System.out.println(fileName.select("wibble/test/hiker.rb"));
     }
 
     @Test
-    void selectFileNameIfFilePathIncludesSpecialCharacterThatIsUnderscore(){
+    void removesUnderscoredSuffixAndExtension(){
         assertEquals(Selection.of(0,5),fileName.select("hiker_Tests.rb"));
 
     }
 
     @Test
-    void selectFileNameIfFilePathIncludesSpecialCharacterThatIsHyphen(){
+    void removesHyphenatedSuffixAndExtension(){
         assertEquals(Selection.of(0,5),fileName.select("hiker-Tests.rb"));
 
     }
 
+    /**
+     * For filenames including multiple "." characters
+     */
     @Test
-    void selectFileNameIfFilePathIncludesSpecialCharacterThatIsFullStop(){
+    void removesDoubleBarrelledExtension(){
         assertEquals(Selection.of(0,5),fileName.select("hiker.Tests.rb"));
 
     }
-
-
-
-//    @Test
-//    void selectFileNameIfInADirectoryAndIncludesKeyWords(){
-//        assertEquals(Selection.of);
-//    }
 }
